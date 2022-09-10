b98b891febba6ff20402ea4c48cfb501748025b9
GiraldoSN <113223541+GiraldoSN@users.noreply.github.com>
2022-09-10 10:32:33 +0700

    Add files via upload

---
+++ tipedata.py
@@ -0,0 +1,27 @@
+
+nama ="Giraldo Nainggolan"
+umur =18
+print ("Nama Saya Adalah" + nama + str(nama))
+# tipe date float
+print ("Nama Saya Adalah" + nama + str(nama))
+print("umur saya adalah : ", umur)
+
+# Tipe List
+
+hewan = ["Ayam", "Kucing", "Kelinci", "Tupai"]
+print(hewan[0])
+print(hewan[1])
+print(hewan[3])
+
+# Tipe Numeric Tambah
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print("penjumlahan : " + str (angka1) + str (angka2) + str (angka3))
+print("perkalian : " + str (angka1) + str (angka2) + str (angka3))
+
+
+
+
+#Tugas menghitung konversi suhu terserah
+#Tugas menghitung bangun ruang
